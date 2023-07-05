9bf8f7f1c3154bf474d09a4f02646985f883a134
migo1 <ertmigot@gmail.com>
2023-07-05 10:48:34 +0300

    style-calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import './calculator.css';
+
+const btnKeys = ['AC', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
 function Calculator() {
-  return <span>Calculator</span>;
+  return (
+    <div className="calculator-container">
+      <div className="screen-display">0.</div>
+      {btnKeys.map((btnKey) => (<Button key={btnKey} btn={btnKey} />))}
+    </div>
+  );
 }
 
 export default Calculator;
