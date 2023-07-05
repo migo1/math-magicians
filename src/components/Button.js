a77f8eeefb1552d8b27b877c809105e540c3ed69
migo1 <ertmigot@gmail.com>
2023-07-05 22:14:32 +0300

    handle-events

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './buttons.css';
 
-function Button({ btn }) {
+function Button({ btn, handleClick }) {
   if (btn === '÷' || btn === '×' || btn === '-' || btn === '+' || btn === '=') {
-    return <div className="button operators">{btn}</div>;
+    return <button type="button" className="button operators" onClick={handleClick}>{btn}</button>;
   }
   if (btn === '0') {
-    return <div className="button other-btns zero-btn">{btn}</div>;
+    return (
+      <button
+        type="button"
+        className="button other-btns zero-btn"
+        onClick={handleClick}
+      >
+        {btn}
+      </button>
+    );
   }
 
-  return <div className="button other-btns">{btn}</div>;
+  return (
+    <button type="button" className="button other-btns" onClick={handleClick}>
+      {btn}
+    </button>
+  );
 }
 
 Button.propTypes = {
-  btn: PropTypes.string.isRequired,
+  btn: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import './calculator.css';
+import caclulate from '../logic/calculate';
 
 const btnKeys = ['AC', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
 function Calculator() {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+
+  const { total, next, operation } = state;
+
+  const handleClick = (e) => {
+    const btnValue = e.target.innerHTML;
+    setState(caclulate(state, btnValue));
+  };
   return (
     <div className="calculator-container">
-      <div className="screen-display">0.</div>
-      {btnKeys.map((btnKey) => (<Button key={btnKey} btn={btnKey} />))}
+      <div className="screen-display">{ total || next || operation || 0 }</div>
+      {btnKeys.map((btnKey) => (<Button key={btnKey} btn={btnKey} handleClick={handleClick} />))}
     </div>
   );
 }
