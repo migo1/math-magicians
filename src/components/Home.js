53d491f48f928133d7a8f266d3a25589aecccdf2
migo1 <ertmigot@gmail.com>
2023-07-11 23:55:10 +0300

    add-routes

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to our page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur</p>
+    </div>
+  );
+}
+
+export default Home;
